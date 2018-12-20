3b2fc4310f29393bc67d5df308fda658aebdcfed
Vlad <vlad.bragoi@gmail.com>
2018-12-20 18:33:20 +0100

    removed unused files

--- Solutions/Continuous_Subsystem/tdf/rectifier.cpp
+++
@@ -1,9 +0,0 @@
-#include "rectifier.h"
-
-rectifier::rectifier(sc_core::sc_module_name _name) :
-        in("in"), out("out") {
-}
-
-void rectifier::processing() {
-    out.write(std::abs(in.read()));
-}
